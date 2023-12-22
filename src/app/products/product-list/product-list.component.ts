9a35a4dd30a7fef8dbe5e3a948f1dd87e799474b
Pedro Papini <ppapinia@gmail.com>
2023-12-22 16:59:13 -0300

    chapter 5 - structural directives

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products= ['Webcam', 'Microphone', 'Wireless Keyboard'];
   selectedProduct = '';
 
   onBuy(name: string) {
