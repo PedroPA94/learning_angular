e6ab9e27d044e3336eccd7870c9c538273886020
Pedro Papini <ppapinia@gmail.com>
2023-12-26 17:59:34 -0300

    chapter 5: custom pipes

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Product } from '../product';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  products= ['Webcam', 'Microphone', 'Wireless Keyboard'];
-  selectedProduct = '';
+export class ProductListComponent implements AfterViewInit {
+  products: Product[] = [
+    { name: 'Webcam', price: 100 },
+    { name: 'Microphone', price: 200 },
+    { name: 'Wireless keyboard', price: 85 }
+  ];
+  selectedProduct: Product | undefined;
+
+  @ViewChild(ProductDetailComponent)
+  productDetail: ProductDetailComponent | undefined;
+
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.product);
+    }
+  }
 
   onBuy(name: string) {
     window.alert(`You just bought ${name}!`);
