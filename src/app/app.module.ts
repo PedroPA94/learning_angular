0e206432fbc7260636ef1b7b5e17fe54e3be3028
Pedro Papini <ppapinia@gmail.com>
2023-12-28 19:32:00 -0300

    chapter 5 - custom directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { CopyrightDirective } from './copyright.directive';
+import { NumericDirective } from './numeric.directive';
+import { PermissionDirective } from './permission.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CopyrightDirective, NumericDirective, PermissionDirective],
   imports: [BrowserModule, ProductsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
