ec5b2b6eb08dad4e08521c6d9ef209d58fff3ef8
Pedro Papini <ppapinia@gmail.com>
2024-01-15 20:27:58 -0300

    chapter 6 - root and component injectors

---
+++ src/app/products/favorites/favorites.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.component.html',
+  styleUrls: ['./favorites.component.css']
+})
+export class FavoritesComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+  }
+}
